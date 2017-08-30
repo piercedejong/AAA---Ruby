34f80b38ac56206146d8e8d9b5a3cf5693094867
Pierce de Jong <pierce.dejong45@gmail.com>
2017-08-29 22:41:28 -0600

    Removed git merge stuff

--- app/controllers/nations_controller.rb
+++ app/controllers/nations_controller.rb
@@ -2,10 +2,6 @@ class NationsController < ApplicationController
 
   def show
     @nation = current_game.nations.find_by(uuid: params[:id])
-<<<<<<< HEAD
-    binding.pry
-=======
->>>>>>> updates
   end
 
 end
