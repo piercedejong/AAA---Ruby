143000980a1f26a0e8e357b8d84334622e061b51
Pierce de Jong <pierce.dejong45@gmail.com>
2017-09-04 19:02:51 -0600

    Updated Eco Nation when changing Nat obj Page

--- app/controllers/nations_controller.rb
+++ app/controllers/nations_controller.rb
@@ -2,14 +2,7 @@ class NationsController < ApplicationController
 
   def show
     @nation = current_game.nations.find_by(uuid: params[:id])
-  end
-
-  def previous
-    @uuid = params[:uuid]
-    current_game.update(eco: current_game.eco-1)
-    if current_game.eco<0
-      current_game.update(eco: current_game.nations.last.nid)
-    end
-    redirect_to nation_path()
+    current_game.update(eco: @nation.nid)
+    binding.pry
   end
 end
