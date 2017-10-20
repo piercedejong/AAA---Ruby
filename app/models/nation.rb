5f0956bfd4ef95e72fba2fc99d782bedf38c46f4
Pierce de Jong <pierce.dejong45@gmail.com>
2017-10-19 19:45:11 -0600

    Working on 1914 version

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -60,11 +60,27 @@ class Nation < ApplicationRecord
 
     def self.create_1942(game_uuid)
       nations = [
-        {nid: 0,name: 'USSR',          roundel: 'ussr.png',   color: '#872323',colorL: '#d55d5d',bank: 24,income: 24},
-        {nid: 1,name: 'Germany',       roundel: 'germany.png',color: '#282828',colorL: '#8a8d8f',bank: 41,income: 41},
-        {nid: 2,name: 'United Kingdom',roundel: 'britain.png',color: '#d4ac80',colorL: '#e4ceb4',bank: 31,income: 31},
-        {nid: 3,name: 'Japan',         roundel: 'japan.png',  color: '#b56f2f',colorL: '#e0b185',bank: 30,income: 30},
-        {nid: 4,name: 'USA',           roundel: 'usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 42,income: 42},
+        {nid: 0,name: 'USSR',   roundel: 'ussr.png',   color: '#872323',colorL: '#d55d5d',bank: 24,income: 24},
+        {nid: 1,name: 'Germany',roundel: 'germany.png',color: '#282828',colorL: '#8a8d8f',bank: 41,income: 41},
+        {nid: 2,name: 'Britain',roundel: 'britain.png',color: '#d4ac80',colorL: '#e4ceb4',bank: 31,income: 31},
+        {nid: 3,name: 'Japan',  roundel: 'japan.png',  color: '#b56f2f',colorL: '#e0b185',bank: 30,income: 30},
+        {nid: 4,name: 'USA',    roundel: 'usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 42,income: 42},
+      ]
+      nations.each do |n|
+        Game.find_by(uuid: game_uuid).nations.create(n)
+      end
+    end
+
+    def self.create_1914(game_uuid)
+      nations = [
+        {nid: 0,name: 'Austria',       roundel: 'ussr.png',   color: '#40bf40',colorL: '#8cd98c',bank: 24,income: 24},
+        {nid: 1,name: 'Russia',        roundel: 'germany.png',color: '#872323',colorL: '#d55d5d',bank: 41,income: 41},
+        {nid: 2,name: 'Germany',       roundel: 'britain.png',color: '#282828',colorL: '#8a8d8f',bank: 31,income: 31},
+        {nid: 3,name: 'France',        roundel: 'japan.png',  color: '#3e6d91',colorL: '#a6c3d9',bank: 30,income: 30},
+        {nid: 4,name: 'Britain',       roundel: 'usa.png',    color: '#d4ac80',colorL: '#e4ceb4',bank: 42,income: 42},
+        {nid: 5,name: 'Ottoman Empire',roundel: 'usa.png',    color: '#248f8f',colorL: '#80ffff',bank: 42,income: 42},
+        {nid: 6,name: 'Italy',         roundel: 'usa.png',    color: '#754100',colorL: '#995f16',bank: 42,income: 42},
+        {nid: 7,name: 'U.S.A',         roundel: 'usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 42,income: 42},
       ]
       nations.each do |n|
         Game.find_by(uuid: game_uuid).nations.create(n)
