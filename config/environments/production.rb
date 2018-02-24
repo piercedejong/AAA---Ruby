9c4c58bab611b131f27bf7062028d48161af8c95
Pierce de Jong <pierce.dejong45@gmail.com>
2018-02-23 23:02:44 -0700

    Main Images not working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  # CAHNGE TO TRUE FOR IMAGES DO NOT KNOW WHY THIS WORKS
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
