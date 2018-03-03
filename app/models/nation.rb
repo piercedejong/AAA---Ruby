175739e56d78a83fa76acb1bb1c6a9df215afba0
Pierce de Jong <pierce.dejong45@gmail.com>
2018-03-02 19:59:16 -0700

    Moved images into folders

--- app/models/nation.rb
+++ app/models/nation.rb
@@ -59,27 +59,27 @@ class Nation < ApplicationRecord
     def self.create_1940(game)
       nations = [
         # OLD COLOURS
-        #{nid: 0,name: 'Germany',roundel: 'germany.png',color: '#282828',colorL: '#8a8d8f',bank: 30,income: 30},
-        #nid: 1,name: 'USSR',   roundel: 'ussr.png',   color: '#872323',colorL: '#d55d5d',bank: 37,income: 37},
-        #{nid: 2,name: 'Japan',  roundel: 'japan.png',  color: '#a2642a',colorL: '#e5be9a',bank: 26,income: 26},
-        #{nid: 3,name: 'USA',    roundel: 'usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 52,income: 52},
-        #{nid: 4,name: 'China',  roundel: 'china.png',  color: '#bdb036',colorL: '#ddd588',bank: 12,income: 12},
-        #{nid: 5,name: 'Britain',roundel: 'britain.png',color: '#d4ac80',colorL: '#e4ceb4',bank: 28,income: 28},
-        #{nid: 6,name: 'Pacific',roundel: 'pacific.png',color: '#d4ac80',colorL: '#e4ceb4',bank: 17,income: 17},
-        #{nid: 7,name: 'Italy',  roundel: 'italy.png',  color: '#754100',colorL: '#995f16',bank: 10,income: 10},
-        #{nid: 8,name: 'ANZAC',  roundel: 'anzac.png',  color: '#87746d',colorL: '#c6bcb8',bank: 10,income: 10},
-        #{nid: 9,name: 'France', roundel: 'france.png', color: '#3e6d91',colorL: '#a6c3d9',bank: 19,income: 19},
+        #{nid: 0,name: 'Germany',roundel: 'roundels/germany.png',color: '#282828',colorL: '#8a8d8f',bank: 30,income: 30},
+        #nid: 1,name: 'USSR',   roundel: 'roundels/ussr.png',   color: '#872323',colorL: '#d55d5d',bank: 37,income: 37},
+        #{nid: 2,name: 'Japan',  roundel: 'roundels/japan.png',  color: '#a2642a',colorL: '#e5be9a',bank: 26,income: 26},
+        #{nid: 3,name: 'USA',    roundel: 'roundels/usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 52,income: 52},
+        #{nid: 4,name: 'China',  roundel: 'roundels/china.png',  color: '#bdb036',colorL: '#ddd588',bank: 12,income: 12},
+        #{nid: 5,name: 'Britain',roundel: 'roundels/britain.png',color: '#d4ac80',colorL: '#e4ceb4',bank: 28,income: 28},
+        #{nid: 6,name: 'Pacific',roundel: 'roundels/pacific.png',color: '#d4ac80',colorL: '#e4ceb4',bank: 17,income: 17},
+        #{nid: 7,name: 'Italy',  roundel: 'roundels/italy.png',  color: '#754100',colorL: '#995f16',bank: 10,income: 10},
+        #{nid: 8,name: 'ANZAC',  roundel: 'roundels/anzac.png',  color: '#87746d',colorL: '#c6bcb8',bank: 10,income: 10},
+        #{nid: 9,name: 'France', roundel: 'roundels/france.png', color: '#3e6d91',colorL: '#a6c3d9',bank: 19,income: 19},
 
-        {nid: 0,name: 'Germany',roundel: 'germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
-        {nid: 1,name: 'USSR',   roundel: 'ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
-        {nid: 2,name: 'Japan',  roundel: 'japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
-        {nid: 3,name: 'USA',    roundel: 'usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
-        {nid: 4,name: 'China',  roundel: 'china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
-        {nid: 5,name: 'Britain',roundel: 'britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
-        {nid: 6,name: 'Pacific',roundel: 'pacific.png',color: @britain,colorL: @britainL,bank: 17,income: 17},
-        {nid: 7,name: 'Italy',  roundel: 'italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
-        {nid: 8,name: 'ANZAC',  roundel: 'anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
-        {nid: 9,name: 'France', roundel: 'france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
+        {nid: 0,name: 'Germany',roundel: 'roundels/germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
+        {nid: 1,name: 'USSR',   roundel: 'roundels/ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
+        {nid: 2,name: 'Japan',  roundel: 'roundels/japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
+        {nid: 3,name: 'USA',    roundel: 'roundels/usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
+        {nid: 4,name: 'China',  roundel: 'roundels/china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
+        {nid: 5,name: 'Britain',roundel: 'roundels/britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
+        {nid: 6,name: 'Pacific',roundel: 'roundels/pacific.png',color: @britain,colorL: @britainL,bank: 17,income: 17},
+        {nid: 7,name: 'Italy',  roundel: 'roundels/italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
+        {nid: 8,name: 'ANZAC',  roundel: 'roundels/anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
+        {nid: 9,name: 'France', roundel: 'roundels/france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
       ]
       nations.each do |n|
         game.nations.create(n)
@@ -88,15 +88,15 @@ class Nation < ApplicationRecord
 
     def self.create_1940_one_eco(game)
       nations = [
-        {nid: 0,name: 'Germany',roundel: 'germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
-        {nid: 1,name: 'USSR',   roundel: 'ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
-        {nid: 2,name: 'Japan',  roundel: 'japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
-        {nid: 3,name: 'USA',    roundel: 'usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
-        {nid: 4,name: 'China',  roundel: 'china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
-        {nid: 5,name: 'Britain',roundel: 'britain.png',color: @britain,colorL: @britainL,bank: 45,income: 45},
-        {nid: 6,name: 'Italy',  roundel: 'italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
-        {nid: 7,name: 'ANZAC',  roundel: 'anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
-        {nid: 8,name: 'France', roundel: 'france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
+        {nid: 0,name: 'Germany',roundel: 'roundels/germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
+        {nid: 1,name: 'USSR',   roundel: 'roundels/ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
+        {nid: 2,name: 'Japan',  roundel: 'roundels/japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
+        {nid: 3,name: 'USA',    roundel: 'roundels/usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
+        {nid: 4,name: 'China',  roundel: 'roundels/china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
+        {nid: 5,name: 'Britain',roundel: 'roundels/britain.png',color: @britain,colorL: @britainL,bank: 45,income: 45},
+        {nid: 6,name: 'Italy',  roundel: 'roundels/italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
+        {nid: 7,name: 'ANZAC',  roundel: 'roundels/anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
+        {nid: 8,name: 'France', roundel: 'roundels/france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
       ]
       nations.each do |n|
         game.nations.create(n)
@@ -105,12 +105,12 @@ class Nation < ApplicationRecord
 
     def self.create_1940_europe(game)
       nations = [
-        {nid: 0,name: 'Germany',roundel: 'germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
-        {nid: 1,name: 'USSR',   roundel: 'ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
-        {nid: 2,name: 'USA',    roundel: 'usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
-        {nid: 3,name: 'Britain',roundel: 'britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
-        {nid: 4,name: 'Italy',  roundel: 'italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
-        {nid: 5,name: 'France', roundel: 'france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
+        {nid: 0,name: 'Germany',roundel: 'roundels/germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
+        {nid: 1,name: 'USSR',   roundel: 'roundels/ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
+        {nid: 2,name: 'USA',    roundel: 'roundels/usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
+        {nid: 3,name: 'Britain',roundel: 'roundels/britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
+        {nid: 4,name: 'Italy',  roundel: 'roundels/italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
+        {nid: 5,name: 'France', roundel: 'roundels/france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
       ]
       nations.each do |n|
         game.nations.create(n)
@@ -119,11 +119,11 @@ class Nation < ApplicationRecord
 
     def self.create_1940_pacific(game)
       nations = [
-        {nid: 0,name: 'Japan',  roundel: 'japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
-        {nid: 1,name: 'USA',    roundel: 'usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
-        {nid: 2,name: 'China',  roundel: 'china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
-        {nid: 3,name: 'Britain',roundel: 'britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
-        {nid: 4,name: 'ANZAC',  roundel: 'anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
+        {nid: 0,name: 'Japan',  roundel: 'roundels/japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
+        {nid: 1,name: 'USA',    roundel: 'roundels/usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
+        {nid: 2,name: 'China',  roundel: 'roundels/china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
+        {nid: 3,name: 'Britain',roundel: 'roundels/britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
+        {nid: 4,name: 'ANZAC',  roundel: 'roundels/anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
       ]
       nations.each do |n|
         game.nations.create(n)
@@ -132,16 +132,16 @@ class Nation < ApplicationRecord
 
     def self.create_1940_grasshopper(game)
       nations = [
-        {nid: 0,name: 'Germany',roundel: 'germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
-        {nid: 1,name: 'USSR',   roundel: 'ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
-        {nid: 2,name: 'Japan',  roundel: 'japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
-        {nid: 3,name: 'USA',    roundel: 'usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
-        {nid: 4,name: 'China',  roundel: 'china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
-        {nid: 5,name: 'Britain',roundel: 'britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
-        {nid: 6,name: 'Pacific',roundel: 'pacific.png',color: @britain,colorL: @britainL,bank: 17,income: 17},
-        {nid: 7,name: 'Italy',  roundel: 'italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
-        {nid: 8,name: 'ANZAC',  roundel: 'anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
-        {nid: 9,name: 'France', roundel: 'france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
+        {nid: 0,name: 'Germany',roundel: 'roundels/germany.png',color: @germany,colorL: @germanyL,bank: 30,income: 30},
+        {nid: 1,name: 'USSR',   roundel: 'roundels/ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 37,income: 37},
+        {nid: 2,name: 'Japan',  roundel: 'roundels/japan.png',  color: @japan,  colorL: @japanL,  bank: 26,income: 26},
+        {nid: 3,name: 'USA',    roundel: 'roundels/usa.png',    color: @usa,    colorL: @usaL,    bank: 52,income: 52},
+        {nid: 4,name: 'China',  roundel: 'roundels/china.png',  color: @china,  colorL: @chinaL,  bank: 12,income: 12},
+        {nid: 5,name: 'Britain',roundel: 'roundels/britain.png',color: @britain,colorL: @britainL,bank: 28,income: 28},
+        {nid: 6,name: 'Pacific',roundel: 'roundels/pacific.png',color: @britain,colorL: @britainL,bank: 17,income: 17},
+        {nid: 7,name: 'Italy',  roundel: 'roundels/italy.png',  color: @italy,  colorL: @italyL,  bank: 10,income: 10},
+        {nid: 8,name: 'ANZAC',  roundel: 'roundels/anzac.png',  color: @anzac,  colorL: @anzacL,  bank: 10,income: 10},
+        {nid: 9,name: 'France', roundel: 'roundels/france.png', color: @france, colorL: @franceL, bank: 19,income: 19},
       ]
       nations.each do |n|
         game.nations.create(n)
@@ -150,11 +150,11 @@ class Nation < ApplicationRecord
 
     def self.create_1942(game)
       nations = [
-        {nid: 0,name: 'USSR',   roundel: 'ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 24,income: 24},
-        {nid: 1,name: 'Germany',roundel: 'germany.png',color: @germany,colorL: @germanyL,bank: 41,income: 41},
-        {nid: 2,name: 'Britain',roundel: 'britain.png',color: @britain,colorL: @britainL,bank: 31,income: 31},
-        {nid: 3,name: 'Japan',  roundel: 'japan.png',  color: @japan,  colorL: @japanL,  bank: 30,income: 30},
-        {nid: 4,name: 'USA',    roundel: 'usa.png',    color: @usa,    colorL: @usaL,    bank: 42,income: 42},
+        {nid: 0,name: 'USSR',   roundel: 'roundels/ussr.png',   color: @ussr,   colorL: @ussrL,   bank: 24,income: 24},
+        {nid: 1,name: 'Germany',roundel: 'roundels/germany.png',color: @germany,colorL: @germanyL,bank: 41,income: 41},
+        {nid: 2,name: 'Britain',roundel: 'roundels/britain.png',color: @britain,colorL: @britainL,bank: 31,income: 31},
+        {nid: 3,name: 'Japan',  roundel: 'roundels/japan.png',  color: @japan,  colorL: @japanL,  bank: 30,income: 30},
+        {nid: 4,name: 'USA',    roundel: 'roundels/usa.png',    color: @usa,    colorL: @usaL,    bank: 42,income: 42},
       ]
       nations.each do |n|
         game.nations.create(n)
@@ -163,14 +163,14 @@ class Nation < ApplicationRecord
 
     def self.create_1914(game)
       nations = [
-        {nid: 0,name: 'Austria',       roundel: 'ussr.png',   color: '#40bf40',colorL: '#8cd98c',bank: 24,income: 24},
-        {nid: 1,name: 'Russia',        roundel: 'germany.png',color: '#872323',colorL: '#d55d5d',bank: 41,income: 41},
-        {nid: 2,name: 'Germany',       roundel: 'britain.png',color: '#282828',colorL: '#8a8d8f',bank: 31,income: 31},
-        {nid: 3,name: 'France',        roundel: 'japan.png',  color: '#3e6d91',colorL: '#a6c3d9',bank: 30,income: 30},
-        {nid: 4,name: 'Britain',       roundel: 'usa.png',    color: '#d4ac80',colorL: '#e4ceb4',bank: 42,income: 42},
-        {nid: 5,name: 'Ottoman Empire',roundel: 'usa.png',    color: '#248f8f',colorL: '#80ffff',bank: 42,income: 42},
-        {nid: 6,name: 'Italy',         roundel: 'usa.png',    color: '#754100',colorL: '#995f16',bank: 42,income: 42},
-        {nid: 7,name: 'USA',         roundel: 'usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 42,income: 42},
+        {nid: 0,name: 'Austria',       roundel: 'roundels/ussr.png',   color: '#40bf40',colorL: '#8cd98c',bank: 24,income: 24},
+        {nid: 1,name: 'Russia',        roundel: 'roundels/germany.png',color: '#872323',colorL: '#d55d5d',bank: 41,income: 41},
+        {nid: 2,name: 'Germany',       roundel: 'roundels/britain.png',color: '#282828',colorL: '#8a8d8f',bank: 31,income: 31},
+        {nid: 3,name: 'France',        roundel: 'roundels/japan.png',  color: '#3e6d91',colorL: '#a6c3d9',bank: 30,income: 30},
+        {nid: 4,name: 'Britain',       roundel: 'roundels/usa.png',    color: '#d4ac80',colorL: '#e4ceb4',bank: 42,income: 42},
+        {nid: 5,name: 'Ottoman Empire',roundel: 'roundels/usa.png',    color: '#248f8f',colorL: '#80ffff',bank: 42,income: 42},
+        {nid: 6,name: 'Italy',         roundel: 'roundels/usa.png',    color: '#754100',colorL: '#995f16',bank: 42,income: 42},
+        {nid: 7,name: 'USA',         roundel: 'roundels/usa.png',    color: '#5a8232',colorL: '#a6cd7e',bank: 42,income: 42},
       ]
       nations.each do |n|
         game.nations.create(n)
