b22ee919154aaf28f46579f2eab497d916aa6a22
Pierce de Jong <pierce.dejong45@gamil.com>
2019-07-18 16:16:51 -0600

    Battle Calcualtor can now update and change game version /battle type

--- app/controllers/battle_calculator_controller.rb
+++ app/controllers/battle_calculator_controller.rb
@@ -8,5 +8,24 @@ class BattleCalculatorController < ApplicationController
   #  (1..6).to_a.shuffle.first
   end
 
-  def simulation; body; end;
+  def update
+    @battle_calculator = current_user.battle_calculator
+    if @battle_calculator.update_attributes(battle_calculator_params)
+      redirect_to battle_calculator_index_path
+    else
+      redirect_to battle_calculator_index_path
+    end
+  end
+
+  def simulation
+
+  end
+
+
+
+
+  private
+    def battle_calculator_params
+      params.require(:battle_calculator).permit(:game_version,:battle_type)
+    end
 end
