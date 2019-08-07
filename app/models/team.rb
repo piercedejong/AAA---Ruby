76768753b445e17eee903b3091540f21474f758a
Pierce de Jong <pierce.dejong45@gamil.com>
2019-08-07 10:18:18 -0600

    dispaly the current amount of units

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,18 @@ class Team < ApplicationRecord
   default_scope { order(created_at: :asc) }
   belongs_to :battle_calculator
   has_many :units
+
+
+
+    def current_units
+      @count = 0
+      self.units.all.each do |u|
+        @count = @count + u.count
+      end
+      return @count
+    end
+
+    def dead_units
+      return self.starting_units-current_units
+    end
 end
